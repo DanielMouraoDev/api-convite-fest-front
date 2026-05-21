c0667933120e7c60df869251b1c96fc78dba9956
DanielMouraoDev <danielmourao777@gmail.com>
2026-05-20 21:02:13 -0300

    Ajuste de configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ajuda o Vite a entender rotas no desenvolvimento
+  },
+  build: {
+    outDir: 'dist', // Garante que a Vercel encontre a pasta de build
+  }
 })
